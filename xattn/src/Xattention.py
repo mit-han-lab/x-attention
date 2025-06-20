fb2ac200a23d20568f7d166ddb5ee247926d2b2b
sa-buc <shanhaikang.shk@oceanbase.com>
2025-06-20 17:48:12 +0800

    fix chunked_prefill_underperforming_issue with use_triton=False
    Signed-off-by: sa-buc <shanhaikang.shk@oceanbase.com>

--- xattn/src/Xattention.py
+++ xattn/src/Xattention.py
@@ -36,6 +36,8 @@ def xattn_estimate(
     k_block_num = (k_len + k_num_to_pad) // block_size
     q_chunk_num = (q_len + q_num_to_pad) // chunk_size
     q_block_num = (q_len + q_num_to_pad) // block_size
+    assert k_chunk_num >= q_chunk_num
+    offset_token_chunk_num = k_chunk_num - q_chunk_num
 
     if k_num_to_pad > 0:
         pad_key_states = F.pad(key_states, (0, 0, 0, k_num_to_pad), value=0).to("cuda")
@@ -197,7 +199,7 @@ def xattn_estimate(
                     device=key_states.device,
                 )
                 causal_mask[:, :, :, (-k_reshaped_num_to_pad):] = float("-inf")
-                chunk_start = chunk_idx * reshaped_chunk_size
+                chunk_start = (chunk_idx + offset_token_chunk_num) * reshaped_chunk_size
                 chunk_end = chunk_start + reshaped_chunk_size
                 causal_mask[:, :, :, chunk_start:chunk_end] = torch.triu(
                     torch.ones(
